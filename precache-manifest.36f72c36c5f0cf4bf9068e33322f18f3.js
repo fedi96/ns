f18140d16fcdcf536f5e83bc3bf8a430733dc57a
fedirjh <fedirjh@gmail.com>
2019-10-14 13:39:02 +0100

    1st push

---
+++ precache-manifest.36f72c36c5f0cf4bf9068e33322f18f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd2dea1bd0edadf011f599b912352814",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99195cab19533c6229c6",
+    "url": "/static/css/2.62908225.chunk.css"
+  },
+  {
+    "revision": "c01fd40d86708ad481c9",
+    "url": "/static/css/main.78853933.chunk.css"
+  },
+  {
+    "revision": "99195cab19533c6229c6",
+    "url": "/static/js/2.2e9bc7eb.chunk.js"
+  },
+  {
+    "revision": "c01fd40d86708ad481c9",
+    "url": "/static/js/main.04a1df9b.chunk.js"
+  },
+  {
+    "revision": "d021ebc9dca79a6f18e9",
+    "url": "/static/js/runtime~main.4338c1d4.js"
+  }
+]);
